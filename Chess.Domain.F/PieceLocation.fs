89aeeb4bd83ece336670fbf1891c6b820d164df2
Svya <svya.gamedeveloper@gmail.com>
2026-09-12 00:54:15 +0300

    Rename member functions for coordinate conversion

--- Chess.Domain.F/PieceLocation.fs
+++ Chess.Domain.F/PieceLocation.fs
@@ -5,15 +5,15 @@ type public PieceLocation = {
     File: uint32
     Rank: uint32
 } with
-    member internal this.roomCoordinates = {
+    member internal this.ToRoomCoordinates = {
         A = uint8 (this.File / 4u)
         B = uint8 (this.Rank / 4u)
     }
-    member internal this.homeCoordinates = {
+    member internal this.ToHomeCoordinates = {
         X = uint64 this.File * 2UL
         Y = uint64 this.Rank * 2UL
     }
-    member internal this.tileCoordinates = {
+    member internal this.ToTileCoordinates = {
         C = this.File
         R = this.Rank
     }
