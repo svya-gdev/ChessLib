6457098ff318f3b51bd4d2024e1604fec0551c15
Svya <svya.gamedeveloper@gmail.com>
2026-09-12 00:57:11 +0300

    Rename methods to follow PascalCase convention

--- Chess.Domain.F/PieceRelocation.fs
+++ Chess.Domain.F/PieceRelocation.fs
@@ -5,15 +5,15 @@ type public PieceRelocation = {
     FileDelta: uint32
     RankDelta: uint32
 } with
-    member internal this.applyTo location = {
+    member internal this.ApplyTo location = {
         File = location.File + this.FileDelta
         Rank = location.Rank + this.RankDelta
     }
-    member internal this.isHorseLike =
+    member internal this.IsHorseLike =
         this.FileDelta <> this.RankDelta &&
         this.FileDelta <> 0u             &&
                     0u <> this.RankDelta
-    member internal this.isStraight = not this.isHorseLike
-    member internal this.isNonMoving =
+    member internal this.IsStraight = not this.IsHorseLike
+    member internal this.IsNonMoving =
         this.FileDelta =  0u &&
         this.RankDelta =  0u
