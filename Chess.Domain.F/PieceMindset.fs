0066510e6f82b71026196d527f16f72169e42b37
Svya <svya.gamedeveloper@gmail.com>
2026-09-12 00:55:31 +0300

    Rename method isHostileTo to IsHostileTo

--- Chess.Domain.F/PieceMindset.fs
+++ Chess.Domain.F/PieceMindset.fs
@@ -5,5 +5,5 @@ type public PieceMindset = {
     Team: Team
     Feud: Feud
 } with
-    member internal this.isHostileTo mindset =
-        this.Feud.isTeam1HostileToTeam2 this.Team mindset.Team
+    member internal this.IsHostileTo mindset =
+        this.Feud.IsTeam1HostileToTeam2 this.Team mindset.Team
