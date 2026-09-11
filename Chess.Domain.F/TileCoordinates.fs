7fe6dcc434dab0d31b83b381ff1569145610f1a7
Svya <svya.gamedeveloper@gmail.com>
2026-09-12 00:59:30 +0300

    Rename coordinate conversion methods to PascalCase

--- Chess.Domain.F/TileCoordinates.fs
+++ Chess.Domain.F/TileCoordinates.fs
@@ -5,11 +5,11 @@ type internal TileCoordinates = {
     C : uint32
     R : uint32
 } with
-    member internal this.toRoomCoordinates = {
+    member internal this.ToRoomCoordinates = {
         A = uint8 (this.C / 4u)
         B = uint8 (this.R / 4u)
     }
-    member internal this.toHomeCoordinates = {
+    member internal this.ToHomeCoordinates = {
         X = uint64 this.C * 2UL
         Y = uint64 this.R * 2UL
     }
